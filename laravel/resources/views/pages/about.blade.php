e530407c9e8a019a87df825ceef2a6c272e3b45a
ChristianPucci91 <pucci.christian91@gmail.com>
2021-01-25 15:40:06 +0100

    test anchors links & css styles

--- laravel/resources/views/pages/about.blade.php
+++ laravel/resources/views/pages/about.blade.php
@@ -2,9 +2,13 @@
 
 @section('content')
 
-<h1>You are in the About page</h1>
 
-<p>Click<a href="{{ route('blog')}}"> <strong> Here </strong></a>if you want to go in the Blog page</p>
-<p>Click<a href="{{ route('home')}}"> <strong> Here </strong></a>if you want to go in the Home page</p>
+<div class="about">
+
+  <h1>You are in the About page</h1>
+  <p>Click<a href="{{ route('blog')}}"> <strong> Here </strong></a>if you want to go in the Blog page</p>
+  <p>Click<a href="{{ route('home')}}"> <strong> Here </strong></a>if you want to go in the Home page</p>
+
+</div>
 
 @endsection
--- laravel/resources/views/pages/home.blade.php
+++ laravel/resources/views/pages/home.blade.php
@@ -2,8 +2,11 @@
 
 @section('content')
 
-<h1>You are in the Home page</h1>
-<p>Click<a href="{{ route('blog')}}"> <strong> Here </strong></a>if you want to go in the Blog page</p>
-<p>Click<a href="{{ route('about')}}"> <strong> Here </strong></a>if you want to go in the About page</p>
+
+<div class="home">
+  <h1>You are in the Home page</h1>
+  <p>Click<a href="{{ route('blog')}}"> <strong> Here </strong></a>if you want to go in the Blog page</p>
+  <p>Click<a href="{{ route('about')}}"> <strong> Here </strong></a>if you want to go in the About page</p>
+</div>
 
 @endsection
