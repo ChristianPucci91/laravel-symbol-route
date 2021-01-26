8e67ae36d4c98a279aa414003ae468ac89d135ef
ChristianPucci91 <pucci.christian91@gmail.com>
2021-01-26 14:34:04 +0100

    Parte esercizio 1 completata

--- laravel2/resources/views/pages/pagamenti.blade.php
+++ laravel2/resources/views/pages/pagamenti.blade.php
@@ -2,6 +2,30 @@
 
 @section('content')
 
-<h1>test</h1>
+<h1>Pagamenti</h1>
+
+@foreach ($pagamenti as $pagamento)
+
+  @if ($loop -> even)
+
+    <div class="pagamenti green">
+
+  @else
+
+    <div class="pagamenti red">
+
+  @endif
+
+    <p><strong>Id: </strong>{{ $pagamento['id']}}</p>
+    <p><strong>Status: </strong>{{ $pagamento['status']}}</p>
+    <p><strong>Price: </strong>{{ $pagamento['price']}}</p>
+    <p><strong>Prenotazione Id: </strong>{{ $pagamento['prenotazione_id']}}</p>
+    <p><strong>Pagante Id: </strong>{{ $pagamento['pagante_id']}}</p>
+    <p><strong>Creata alle ore: </strong>{{ $pagamento['created_at']}}</p>
+    <p><strong>Aggiornata alle ore: </strong>{{ $pagamento['updated_at']}}</p>
+
+  </div>
+
+@endforeach
 
 @endsection
